758af837cc94f5c4c08489684c57b2249e14714c
basharTezda <164339899+basharTezda@users.noreply.github.com>
2025-05-09 20:44:46 +0300

    push update

--- lib/tezda_ios_player.dart
+++ lib/tezda_ios_player.dart
@@ -1,36 +1,4 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:tezda_ios_player/controller.dart';
-
+export 'package:tezda_ios_player/src/player_slider.dart';
 export 'package:tezda_ios_player/tezda_ios_player.dart';
-export 'package:tezda_ios_player/controller.dart';
-
-class NativeVideoWidget extends StatelessWidget {
-  final 
-  String url;  // Accept a list of URLs
-  final NativeVideoController controller;
-  final bool shouldMute;
-  final bool isLandscape;
-
-  const NativeVideoWidget({
-    super.key,
-    required this.url,
-    required this.controller,
-    required this.shouldMute,
-    required this.isLandscape,
-  });
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        UiKitView(
-          viewType: 'native_video_player',
-          creationParams: {'url': url, 'shouldMute': shouldMute, 'isLandscape': isLandscape},
-          creationParamsCodec: const StandardMessageCodec(),
-        ),
-      ],
-    );
-  }
-}
+export 'package:tezda_ios_player/src/player_controller.dart';
+export 'package:tezda_ios_player/src/player_view.dart';
