16cc8ae6d6cc6f7e42f382b7a60ab66f223bc495
afrah-tezda <afrah@tezda.com>
2025-05-16 22:01:49 +0200

    fixed padding

--- lib/src/player_slider.dart
+++ lib/src/player_slider.dart
@@ -19,29 +19,31 @@ class __NativeVideoSliderStateState extends State<NativeVideoSlider> {
 
   @override
   Widget build(BuildContext context) {
-    return Slider(
-      thumbColor: Colors.red.withOpacity(0.0),
-
-      padding: EdgeInsets.all(0),
-      activeColor: Color.fromRGBO(255, 255, 255, 1),
-      inactiveColor: Colors.transparent,
-      secondaryTrackValue:
-          (NativeVideoController.buffered.inMicroseconds /
-                      NativeVideoController.duration.inMicroseconds) >
-                  1
-              ? 1
-              : NativeVideoController.buffered.inMicroseconds /
-                  NativeVideoController.duration.inMicroseconds,
-      secondaryActiveColor: Colors.grey,
-      value:
-          NativeVideoController.currentTime.inMicroseconds /
-          NativeVideoController.duration.inMicroseconds,
-      onChanged: (double value) {
-        NativeVideoController.seekTo(
-          value * NativeVideoController.duration.inMicroseconds / 1000000,
-        );
-
-      },
+    return Padding(
+      padding: const EdgeInsets.all(0),
+      child: Slider(
+        thumbColor: Colors.red.withOpacity(0.0),
+       // padding: EdgeInsets.all(0),
+        activeColor: Color.fromRGBO(255, 255, 255, 1),
+        inactiveColor: Colors.transparent,
+        secondaryTrackValue:
+            (NativeVideoController.buffered.inMicroseconds /
+                        NativeVideoController.duration.inMicroseconds) >
+                    1
+                ? 1
+                : NativeVideoController.buffered.inMicroseconds /
+                    NativeVideoController.duration.inMicroseconds,
+        secondaryActiveColor: Colors.grey,
+        value:
+            NativeVideoController.currentTime.inMicroseconds /
+            NativeVideoController.duration.inMicroseconds,
+        onChanged: (double value) {
+          NativeVideoController.seekTo(
+            value * NativeVideoController.duration.inMicroseconds / 1000000,
+          );
+      
+        },
+      ),
     );
   }
 }
