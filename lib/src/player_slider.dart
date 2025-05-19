5ff3dfd8f5bad6124f21654d29471ef29c5038cb
Bashar Albashier <47083066+basharalbashier@users.noreply.github.com>
2025-05-19 12:43:35 +0300

    ..

--- lib/src/player_slider.dart
+++ lib/src/player_slider.dart
@@ -1,4 +1,7 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_xlider/flutter_xlider.dart';
 import 'player_controller.dart';
 
 class NativeVideoSlider extends StatefulWidget {
@@ -19,27 +22,47 @@ class __NativeVideoSliderStateState extends State<NativeVideoSlider> {
 
   @override
   Widget build(BuildContext context) {
-    return Slider(
-      thumbColor: Colors.red.withOpacity(0.0),
-      // padding: EdgeInsets.all(0),
-      activeColor: Color.fromRGBO(255, 255, 255, 1),
-      inactiveColor: Colors.transparent,
-      secondaryTrackValue: (NativeVideoController.buffered.inMicroseconds /
-                  NativeVideoController.duration.inMicroseconds) >
-              1
-          ? 1
-          : NativeVideoController.buffered.inMicroseconds /
-              NativeVideoController.duration.inMicroseconds,
-      secondaryActiveColor: Colors.grey,
-      value: NativeVideoController.currentTime.inMicroseconds /
-    
-          NativeVideoController.duration.inMicroseconds,
-      onChanged: (double value) {
-        NativeVideoController.seekTo(
-          value * NativeVideoController.duration.inMicroseconds / 1000000,
-        );
-    
+    return FlutterSlider(
+      values: [NativeVideoController.currentTime.inMicroseconds.ceilToDouble()],
+      max: NativeVideoController.duration.inMicroseconds.ceilToDouble(),
+      min: 0,
+      handler: FlutterSliderHandler(
+        opacity: 0,
+        disabled: true,
+        child: Icon(Icons.circle, size: 5),
+      ),
+      trackBar: FlutterSliderTrackBar(
+        activeTrackBar: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(4)),
+        inactiveTrackBar: BoxDecoration(
+            color: Colors.grey, borderRadius: BorderRadius.circular(4)),
+      ),
+      onDragging: (handlerIndex, lowerValue, upperValue) {
+        log(lowerValue.toString());
+        // Call Swift native code via MethodChannel
+        // _channel.invokeMethod('seekTo', {'progress': lowerValue});
       },
+
+      // thumbColor: Colors.red.withOpacity(0.0),
+      // // padding: EdgeInsets.all(0),
+      // activeColor: Color.fromRGBO(255, 255, 255, 1),
+      // inactiveColor: Colors.transparent,
+      // secondaryTrackValue: (NativeVideoController.buffered.inMicroseconds /
+      //             NativeVideoController.duration.inMicroseconds) >
+      //         1
+      //     ? 1
+      //     : NativeVideoController.buffered.inMicroseconds /
+      //         NativeVideoController.duration.inMicroseconds,
+      // secondaryActiveColor: Colors.grey,
+      // value: NativeVideoController.currentTime.inMicroseconds /
+
+      //     NativeVideoController.duration.inMicroseconds,
+      // onChanged: (double value) {
+      //   NativeVideoController.seekTo(
+      //     value * NativeVideoController.duration.inMicroseconds / 1000000,
+      //   );
+
+      // },
     );
   }
 }
